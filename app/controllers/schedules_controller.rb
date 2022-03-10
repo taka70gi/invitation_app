ed31bf94c50fa452e75eae5e2b01677ec7a991a4
taka70gi <taka.70.gi@gmail.com>
2022-03-10 22:11:20 +0900

    [add]カレンダー表示画面作成
    カレンダーが表示されるようにviewを作成しました。

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,19 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Schedule.all
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    Schedule.create(schedule_parameter)
+    redirect_to root_path
+  end
+
+  private
+  def schedule_parameter
+    params.require(:schedule).permit(:title, :content, :start_time)
   end
 end
