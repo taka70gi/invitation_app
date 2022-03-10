720062c833ff21621190e8d3d694f06f1ee2beb7
taka70gi <taka.70.gi@gmail.com>
2022-03-10 22:12:58 +0900

    [del]不要ファイル削除
    testなど不要なファイルを削除しました。

--- test/controllers/schedules_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SchedulesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get schedules_index_url
-    assert_response :success
-  end
-end
